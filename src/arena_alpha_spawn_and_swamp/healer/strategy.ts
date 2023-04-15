caa43b2c3e6a86d01f696c5cf65b9f589865afc2
Escalonador <migueljlaluis@gmail.com>
2023-04-15 11:38:45 +0100

    fix(healers): filter targets by existing

--- src/arena_alpha_spawn_and_swamp/healer/strategy.ts
+++ src/arena_alpha_spawn_and_swamp/healer/strategy.ts
@@ -2,7 +2,11 @@ import { state } from 'arena_alpha_spawn_and_swamp/utils/state'
 import { Creep } from 'game/prototypes'
 
 export const findBestHealMatch = (): Creep => {
-    const possibleTargets = state.attackers
+    let possibleTargets = state.attackers.filter(creep => creep.exists)
+
+    if (!possibleTargets) {
+        possibleTargets = state.healers.filter(creep => creep.exists)
+    }
 
     return possibleTargets && possibleTargets.sort((a, b) => a.hits - b.hits)[0]
 }
